b23ff99013a97bbe2455d46c775a3272fb2e6587
Daniel5040 <daniel.dela000@gmail.com>
2021-02-10 13:48:31 -0600

    eslint & prettier stetup, header done

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <nav className="header">
+      <h1 className="title">Make your Resume</h1>
+    </nav>
+  )
+}
+
+export default Header
